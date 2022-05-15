afde194f0170dc367b64ca2b215ed4a880fe5c53
Eric Deng <eric_deng@htc.com>
2022-05-15 14:44:08 +0800

    add quickSortByStack

--- sorting/quick-sort.js
+++ sorting/quick-sort.js
@@ -15,6 +15,26 @@ const quickSort = (arr, left, right) => {
   return arr; // 回傳排序好的arr
 };
 
+const quickSortByStack = (arr, left, right) => {
+  const stack = [];
+  stack.push(left);
+  stack.push(right);
+
+  while (stack.length > 0) {
+    const end = stack.pop();
+    const start = stack.pop();
+
+    if (start < end) {
+      const partitionIndex = partition(arr, start, end);
+      stack.push(start);
+      stack.push(partitionIndex - 1);
+      stack.push(partitionIndex + 1);
+      stack.push(end);
+    }
+  }
+  return arr;
+};
+
 // 挑選一個pivot，把小於pivot的元素移到pivot左區塊，大於等於pivot的元素在右區塊，回傳pivot該排序到的正確位置index，這邊的時間複雜度是N
 const partition = (arr, left, right) => {
   const pivotIndex = left; // 把第一個元素當作pivot
